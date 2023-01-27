4c4274eb6ea3118549ceeea0e0daef52aabb3fda
shehabMm <shehab.mm@gmail.com>
2023-01-27 07:59:20 +0200

    java modified

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,15 @@
 const burgerMenu = document.querySelector(".burger-menu");
-const blackScreen= document.querySelector(".black-screen")
+const blackScreen = document.querySelector(".black-screen")
 const close = document.querySelector(".close");
-const nav  = document.querySelector(".nav");
-
+const nav = document.querySelector(".nav");
+const change = document.querySelector(".change")
+const parent = document.querySelector(".parent")
 burgerMenu.addEventListener("click", (eo) => {
 
-  burgerMenu.style.display="none"
-  blackScreen.style.display="block";
-  close.style.display="block";
+  burgerMenu.style.display = "none"
+  blackScreen.style.display = "block";
+  close.style.display = "block";
+  parent.classList.add("change");
 
 })
 
@@ -15,8 +17,8 @@ burgerMenu.addEventListener("click", (eo) => {
 close.addEventListener("click", (eo) => {
 
 
-  blackScreen.style.display="none";
-  burgerMenu.style.display="block"
+  blackScreen.style.display = "none";
+  burgerMenu.style.display = "block";
+  parent.style.display = "none";
 
-  
 })
