a8342c72d650e3ea2803982e8f8680dfb3aabbe3
shehabMm <shehab.mm@gmail.com>
2023-01-27 07:28:02 +0200

    modified

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,25 @@
 const burgerMenu = document.querySelector(".burger-menu");
 const blackScreen= document.querySelector(".black-screen")
-
+const close = document.querySelector(".close");
 
 
 burgerMenu.addEventListener("click", (eo) => {
 
-  burgerMenu.classList.toggle("dark");
-
+  burgerMenu.style.display="none"
   blackScreen.style.display="block";
+  close.style.display="block";
+  close.style.transform="rotate(450deg)" ;
+  eo.target.style.transition = "all 2s ease";
+  
+
+})
 
 
+close.addEventListener("click", (eo) => {
+
+
+  blackScreen.style.display="none";
+  burgerMenu.style.display="block"
 
   
 })
