c07fc160fad9c36ec22acac1d455ad6624b60da2
shehabMm <shehab.mm@gmail.com>
2023-01-29 08:33:55 +0200

    adding end section

--- portfolio.js
+++ portfolio.js
@@ -11,42 +11,44 @@ const parent = document.querySelector(".parent")
 
 
 
-burgerMenu.addEventListener("click", (eo) => {
 
-  burgerMenu.style.display = "none"
-  close.style.display = "block";
-  parent.classList.add("change");
-  blackScreen.style.display="block";
-})
 
 
 close.addEventListener("click", (eo) => {
 
 
   blackScreen.style.display = "none";
-  burgerMenu.style.display = "block";
   parent.classList.remove("change");
+  burgerMenu.style.display = "block";
 
+})
+
+burgerMenu.addEventListener("click", (eo) => {
+
+  burgerMenu.style.display = "none";
+  close.style.display = "block";
+  parent.classList.add("change");
+  blackScreen.style.display="block";
 })
 
 
 
-// contact me
-function validateForm() {
-  // Get the values from the form
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  var message = document.getElementById('message').value;
+// // contact me
+// function validateForm() {
+//   // Get the values from the form
+//   var name = document.getElementById('name').value;
+//   var email = document.getElementById('email').value;
+//   var message = document.getElementById('message').value;
  
-  // Validate the values
-  if (name == '' || email == '' || message == '') {
-    alert('Please fill out all fields.');
-    return false;
-  }
+//   // Validate the values
+//   if (name == '' || email == '' || message == '') {
+//     alert('Please fill out all fields.');
+//     return false;
+//   }
  
-  // Submit the form
-  return true;
-}
+//   // Submit the form
+//   return true;
+// }
 
 
 
